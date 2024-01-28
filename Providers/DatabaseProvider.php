ab1691a6f7af39220948cad9ecdb8deff8375d85
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-01-28 08:36:06 +0800

    ref: move to src directory

--- Providers/DatabaseProvider.php
+++ Providers/DatabaseProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Inspira\Framework\Providers;
 
-use PDO;
 use Inspira\Config\Config;
 use Inspira\Database\Builder\Query;
 use Inspira\Database\Connection;
@@ -12,6 +11,7 @@ use Inspira\Database\Connectors\MySql;
 use Inspira\Database\Connectors\PgSql;
 use Inspira\Database\Connectors\Sqlite;
 use Inspira\Framework\Application;
+use PDO;
 use Symfony\Component\String\Inflector\EnglishInflector;
 use Symfony\Component\String\Inflector\InflectorInterface;
 
